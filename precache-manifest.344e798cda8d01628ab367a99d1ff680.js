1986308dc4eba02e79743a5d7c45b348f155041e
Nick Walters <nick.walters@argandsolutions.com>
2020-08-19 21:24:29 +0100

    Updates

---
+++ precache-manifest.344e798cda8d01628ab367a99d1ff680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27abe12285e8fbc63af344ccecb92fc1",
+    "url": "/acme-color-app/index.html"
+  },
+  {
+    "revision": "cedbbe3f0c029ecc305c",
+    "url": "/acme-color-app/static/css/main.14424266.chunk.css"
+  },
+  {
+    "revision": "7a778c50a19a98c8fda2",
+    "url": "/acme-color-app/static/js/2.0ed7b24a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/acme-color-app/static/js/2.0ed7b24a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cedbbe3f0c029ecc305c",
+    "url": "/acme-color-app/static/js/main.da8a7ce2.chunk.js"
+  },
+  {
+    "revision": "08fc6f9e9f1a6c3eae60",
+    "url": "/acme-color-app/static/js/runtime-main.d3b0ac55.js"
+  }
+]);
